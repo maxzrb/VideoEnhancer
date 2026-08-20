8cb0fc3f4f34a528ad4fb7deb6b017c389e702ac
ARXChem <105414910+user-Wing@users.noreply.github.com>
2026-08-20 17:17:17 +0800

    Delete VideoEnhancerPlugin directory

--- VideoEnhancerPlugin/build-support/avconfig.h
+++
@@ -1,8 +0,0 @@
-#ifndef AVUTIL_AVCONFIG_H
-#define AVUTIL_AVCONFIG_H
-
-/* Windows x64 configuration used by the matching BtbN shared FFmpeg runtime. */
-#define AV_HAVE_BIGENDIAN 0
-#define AV_HAVE_FAST_UNALIGNED 1
-
-#endif /* AVUTIL_AVCONFIG_H */
